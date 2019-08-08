5fc22b9224d99da8560aac8597aca6061c6aa257
Joel Kerfoot <joelkerfoot@gmail.com>
2019-08-07 20:28:41 -0700

    first commit

---
+++ highest-salary/index.js
@@ -0,0 +1,9 @@
+/*
+ * https://codegolf.stackexchange.com/questions/171168/without-using-numbers-get-the-highest-salary-you-can-but-dont-exaggerate
+ *
+ * Without using numbers, get the highest salary you can. But don't exaggerate!
+ *
+ */
+
+// attempt 1 - 13 bytes
+_ => `$${Math.PI}`;
---
+++ highest-salary/main.py
@@ -0,0 +1,14 @@
+#
+# https://codegolf.stackexchange.com/questions/171168/without-using-numbers-get-the-highest-salary-you-can-but-dont-exaggerate
+#
+# Without using numbers, get the highest salary you can. But don't exaggerate!
+#
+
+# attempt 2 - 17 bytes - $119200
+f"${ord('𝆠')}" # probably needs a print so it can be run in as a file and not from the python cli
+
+# attempt 2.1 - 
+print(f"${ord('𖠿')}")
+
+# attempt 2.3
+print('$',ord('𘂊'))
